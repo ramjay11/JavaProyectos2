e10f5865ee48b80cd894bc8beef0958c0e5f81ce
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2023-09-27 21:09:32 +0800

    adding webflux-demonstration

---
+++ webflux-demonstration/src/main/java/com/ramjava/webflux/demonstration/WebfluxDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.webflux.demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxDemonstrationApplication.class, args);
+	}
+
+}
---
+++ webflux-demonstration/src/main/java/com/ramjava/webflux/demonstration/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.ramjava.webflux.demonstration.controller;
+
+import com.ramjava.webflux.demonstration.entity.Student;
+import com.ramjava.webflux.demonstration.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService service;
+
+    @GetMapping("/synchronous")
+    public List<Student> students() {
+        return service.getStudents();
+    }
+
+    // in reactive programs, we need to send data as an event stream
+    @GetMapping(value = "/asynchronous", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Student> studentList() {
+        return service.getStudentList();
+    }
+}
